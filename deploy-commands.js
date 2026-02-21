a232255cd83fb23c6f1935fa3691105dfa67724f
drxv1122 <167954037+drxv1122@users.noreply.github.com>
2026-02-21 23:18:57 +0530

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,17 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+require('dotenv').config();
+
+const commands = [
+    new SlashCommandBuilder()
+        .setName('setupverify')
+        .setDescription('Creates the verification panel')
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    await rest.put(
+        Routes.applicationCommands("YOUR_CLIENT_ID"),
+        { body: commands }
+    );
+})();
